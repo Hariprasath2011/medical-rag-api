5ad8d2326a006874952e96e592ff31d1c71b0f28
Hariprasath <hariprasathjaganathan@gmail.com>
2026-07-13 10:39:29 +0530

    Update embedding_service.py

--- services/embedding_service.py
+++ services/embedding_service.py
@@ -1,1 +1,8 @@
+from langchain_huggingface import HuggingFaceEmbeddings
 
+embedding_model = HuggingFaceEmbeddings(
+    model_name="BAAI/bge-m3"
+)
+
+def embed_documents(texts):
+    return embedding_model.embed_documents(texts)
