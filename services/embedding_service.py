a28ccf49620e20eb7c0e0d313560628e1024567f
Hariprasath <hariprasathjaganathan@gmail.com>
2026-07-13 12:27:52 +0530

    Delete services/embedding_service.py

--- services/embedding_service.py
+++
@@ -1,8 +0,0 @@
-from langchain_huggingface import HuggingFaceEmbeddings
-
-embedding_model = HuggingFaceEmbeddings(
-    model_name="BAAI/bge-small-en-v1.5"
-)
-
-def embed_documents(texts):
-    return embedding_model.embed_documents(texts)
